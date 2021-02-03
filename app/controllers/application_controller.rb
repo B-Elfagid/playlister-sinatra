e3a1a2ed6155ba2da2aa0976f8e9a16a38c0c95a
B-Elfagid <betty_elfagid@yahoo.co.uk>
2021-02-03 22:44:36 +0000

    commit tree 85a60a3d4e4823c52226aceccb4f3023e4328cce

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < Sinatra::Base
+  require_relative '../config/environment'
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
